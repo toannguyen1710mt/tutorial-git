55aa2b6d07b0cfc9d91d03b0188b4cab60bfc1a1
toannguyen1710mt <toannguyen1710mt@gmail.com>
2019-05-29 23:49:28 +0700

    second commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tutorial title="welcome to my git"></tutorial>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Tutorial from './components/Tutorial'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Tutorial
   }
 }
 </script>
